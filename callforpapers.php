f057aa6470ea71279e7a20eddc95cee2c1ca3436
Dhatchayani <dhatchu1810@gmail.com>
2024-08-26 16:36:44 +0530

    initial commit

---
+++ callforpapers.php
@@ -0,0 +1,55 @@
+<?php include('header.php'); ?>
+<section class="callforpaper-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="callforpaper-container">
+                    <h1>Call for papers</h1>
+                    <p>Search for current calls for papers in your subject area</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="callforpaper-image">
+                    <img src="images/callforpaper.avif" alt="callforpaper" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="call-for-papers-section">
+    <div class="container">
+        <div class="row mb-4">
+            <div class="col-md-8 ">
+                <p>Use our search tool to find call for papers in your subject area, in 2 simple steps.</p>
+
+                <p><strong>Step 1:</strong> Select the relevant check box/es and press ‘Load’.</p>
+                <p><strong>Step 2:</strong> Explore the results across Taylor & Francis, Routledge, and Cogent journals.
+                </p>
+
+                <p>If a particular journal doesn’t show, it doesn’t mean it isn’t accepting submissions. Rather, the
+                    journal is
+                    promoting a special theme or topic for a limited time.</p>
+                <p>If you’re looking to publish open access, we’ve got plenty of information here on Author Services.
+                </p>
+            </div>
+            <div class="col-md-4 call-papers">
+                <h4>Explore alternative call for papers</h4>
+                <div class="alternative-call">
+                    <img src="images/f1000-images.png" alt="F1000Research" class="img-fluid">
+                    <p><a href="#">F1000Research<i class="fa-solid fa-arrow-up-right-from-square"></i></a> is an Open
+                        Research publishing platform for scientists, scholars,
+                        and
+                        clinicians.</p>
+                </div>
+                <div class="alternative-call">
+                    <img src="images/routelege-image.jpeg" alt="F1000Research" class="img-fluid">
+                    <p><a href="#">F1000Research<i class="fa-solid fa-arrow-up-right-from-square"></i></a> is a
+                        platform for the arts, humanities,
+                        behavioural, social sciences</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
