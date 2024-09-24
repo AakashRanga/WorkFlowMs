df4a6aa28bbd6f0f63812e024b0e22bd0ae6d4e7
Dhatchayani <dhatchu1810@gmail.com>
2024-09-24 22:11:29 +0530

    initial commit

---
+++ section/explore-journals.php
@@ -0,0 +1,50 @@
+<?php include("navbar.php"); ?>
+
+
+<section class="explore-section py-5">
+    <div class="container text-center">
+        <!-- Section Title -->
+        <h1 class="mb-4" style="color:black">Explore Journals and Articles by Subject</h1>
+
+        <!-- Section Description -->
+        <p class="lead">
+            Search and explore millions of quality, peer-reviewed journal articles published under the Taylor & Francis,
+            Routledge, and Dove Medical Press imprints.
+        </p>
+    </div>
+    <div class="container-fluid mt-5">
+        <ul>
+            <li><a href="/topic/allsubjects/as">Area Studies</a></li>
+            <li><a href="/topic/allsubjects/ar">Arts</a></li>
+            <li><a href="/topic/allsubjects/be">Behavioral Sciences</a></li>
+            <li><a href="/topic/allsubjects/bs">Bioscience</a></li>
+            <li><a href="/topic/allsubjects/bu">Built Environment</a></li>
+            <li><a href="/topic/allsubjects/cs">Communication Studies</a></li>
+            <li><a href="/topic/allsubjects/cm">Computer Science</a></li>
+            <li><a href="/topic/allsubjects/ea">Earth Sciences</a></li>
+            <li><a href="/topic/allsubjects/eb">Economics, Finance, Business &amp; Industry</a></li>
+            <li><a href="/topic/allsubjects/ed">Education</a></li>
+            <li><a href="/topic/allsubjects/ec">Engineering &amp; Technology</a></li>
+            <li><a href="/topic/allsubjects/ag">Environment &amp; Agriculture</a></li>
+            <li><a href="/topic/allsubjects/es">Environment and Sustainability</a></li>
+            <li><a href="/topic/allsubjects/fs">Food Science &amp; Technology</a></li>
+            <li><a href="/topic/allsubjects/ge">Geography</a></li>
+            <li><a href="/topic/allsubjects/ds">Global Development</a></li>
+            <li><a href="/topic/allsubjects/hs">Health and Social Care</a></li>
+            <li><a href="/topic/allsubjects/hu">Humanities</a></li>
+            <li><a href="/topic/allsubjects/if">Information Science</a></li>
+            <li><a href="/topic/allsubjects/la">Language &amp; Literature</a></li>
+            <li><a href="/topic/allsubjects/lw">Law</a></li>
+            <li><a href="/topic/allsubjects/ma">Mathematics &amp; Statistics</a></li>
+            <li><a href="/topic/allsubjects/me">Medicine, Dentistry, Nursing &amp; Allied Health</a></li>
+            <li><a href="/topic/allsubjects/ah">Museum and Heritage Studies</a></li>
+            <li><a href="/topic/allsubjects/pc">Physical Sciences</a></li>
+            <li><a href="/topic/allsubjects/pi">Politics &amp; International Relations</a></li>
+            <li><a href="/topic/allsubjects/sn">Social Sciences</a></li>
+            <li><a href="/topic/allsubjects/sl">Sports and Leisure</a></li>
+            <li><a href="/topic/allsubjects/sp">Tourism, Hospitality and Events</a></li>
+            <li><a href="/topic/allsubjects/us">Urban Studies</a></li>
+        </ul>
+    </div>
+</section>
+
