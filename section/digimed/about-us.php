73bd9559e91cf87a9476e3ed2b3a4cf28f090739
clarencepraison <clarencepraison32303@gmail.com>
2024-09-28 12:27:57 +0530

    Ak Changes

---
+++ section/digimed/about-us.php
@@ -0,0 +1,27 @@
+<!-- about us section -->
+<section class="about-us py-5">
+        <div class="container">
+
+            <div class="row align-items-center">
+
+                <div class="col-md-6">
+                    <h2 class="text-left text-white">About Us</h2>
+                    <!-- <hr stye="background-color:#050c9c;"> -->
+                    <p class="text-white">
+                    <strong>Digimed Global</strong> is a unique online learning platform designed to enhance the interface and knowledge sharing among researchers. It has revolutionized research education, making it more accessible and flexible. Users can engage with peers globally through online discussion boards, live webinars, and virtual networking events.
+                    </p>
+                    <p class="text-white">
+                    Catering to both graduates, postgraduates, and professionals, Digimed Global offers specialized fellowship courses tailored to the needs of research professionals. We are committed to delivering high-quality education that enhances the knowledge, skills, and career prospects of individuals.
+                    </p>
+                    <p class="text-white">
+                    Our mission is to convey excellent research education globally, strengthening skills, knowledge, and capabilities to handle complex challenges in the research field.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="../../images/education.jpg" alt="About Us" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
---
+++ section/digimed/index.php
@@ -0,0 +1,11 @@
+<?php include ("navbar.php")?>
+
+<?php include("about-us.php")?>
+
+<?php include ("ourservice-section.php")?>
+
+<?php include ("explore-journals.php")?>
+
+<?php include ("publication-section.php")?>
+
+<?php include ("../footer-section.php")?>
