b9f3bf753e4974f14cc920c9985de77c09da61a8
clarencepraison <clarencepraison32303@gmail.com>
2024-09-28 12:27:57 +0530

    Ak Changes

---
+++ section/digimed/explore-journals.php
@@ -0,0 +1,31 @@
+
+
+<section class="explore-section py-5">
+    <div class="container text-center">
+        <!-- Section Title -->
+        <h1 class="mb-4" style="color:black">Certification / FellowShip Features </h1>
+
+        <!-- Section Description -->
+        <p class="lead">
+        Enhance your expertise with flexible, globally recognized programs designed to advance your career and professional growth.
+        </p>
+    </div>
+    <div class="container-fluid mt-5">
+        <ul>
+            <li><a href="/topic/allsubjects/as">Global Accreditation</a></li>
+            <li><a href="/topic/allsubjects/ar">Hassle-Free Admission</a></li>
+            <li><a href="/topic/allsubjects/be">Flawless Lectures</a></li>
+            <li><a href="/topic/allsubjects/bs">Delivery And Assessment</a></li>
+            <li><a href="/topic/allsubjects/bu">Experienced Faculty</a></li>
+            <li><a href="/topic/allsubjects/cs">Fast Career Growth</a></li>
+            <li><a href="/topic/allsubjects/cm">Easy Billing</a></li>
+            <li><a href="/topic/allsubjects/cm">Easy EMI Options</a></li>
+            <li><a href="/topic/allsubjects/ea">Educational Leadership</a></li>
+            <li><a href="/topic/allsubjects/eb">Top Reasearch &amp; Writing Courses</a></li>
+            <li><a href="/topic/allsubjects/ed">Hands-on Training</a></li>
+            <li><a href="/topic/allsubjects/ec">Seminars &amp; Workshops</a></li>
+           
+        </ul>
+    </div>
+</section>
+
