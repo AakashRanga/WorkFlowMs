5291bda5658007e140036b71f5155baebdf8d531
Dhatchayani <dhatchu1810@gmail.com>
2024-10-08 18:38:22 +0530

    dak commited

--- section/digimed/digimed-navbar.php
+++ section/digimed/digimed-navbar.php
@@ -9,7 +9,8 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+     <link rel="stylesheet" href="../../css/bootstrap.min.css">
     <!-- Bootstrap JS with Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="../digimed/css/digimed-style.css" rel="stylesheet">
@@ -48,7 +49,7 @@
                         <a class="nav-link" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item hover-underline-animation">
-                        <a class="nav-link" href="#">Courses</a>
+                        <a class="nav-link" href="courses.php">Courses</a>
                     </li>
                     <li class="nav-item hover-underline-animation">
                         <a class="nav-link" href="contact-us.php">Contact</a>
