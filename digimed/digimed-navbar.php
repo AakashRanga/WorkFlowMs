a9a7b4410c226274eb24fdd67eacb7921dac42f6
Dhatchayani <dhatchu1810@gmail.com>
2024-10-16 14:09:07 +0530

    initial commit

--- digimed/digimed-navbar.php
+++ digimed/digimed-navbar.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
 
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     <!-- Bootstrap JS with Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     
