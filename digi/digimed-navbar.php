d021d924414cc5e30a084bc966bde69906930e0f
Dhatchayani <dhatchu1810@gmail.com>
2024-10-19 14:19:16 +0530

    Dhatchu Changes

--- digi/digimed-navbar.php
+++ digi/digimed-navbar.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WORK FLOW MANAGEMENT SYSTEM</title>
+    <link rel="icon" type="image/png" href="images/digimed-logo.png">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
