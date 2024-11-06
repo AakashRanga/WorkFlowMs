7bd2bb60cb63dcb5bef52a5343a1e714a521b4aa
Dhatchayani <dhatchu1810@gmail.com>
2024-11-06 21:56:54 +0530

    Vmago main

---
+++ vmagomain/index.php
@@ -0,0 +1,14 @@
+<?php include ("vmago-navbar.php")?>
+
+
+<?php include ("vmago-banner.php")?>
+
+<?php include ("knowledege-partners.php")?>
+
+<?php include ("services.php")?>
+
+
+<?php include ("support.php")?>
+
+
+
---
+++ vmagomain/services.php
@@ -0,0 +1,123 @@
+<style>
+     .vmago-section {
+        padding: 30px 0px;
+        background-color: #fff;
+    }
+
+    .vmago-section h3 {
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    .vsection {
+        /* background: linear-gradient(104.98deg,#f4fbfc 11.58%,#f4fcf9 90.7%); Semi-transparent white */
+        background-color: #cccccc38;
+        backdrop-filter: blur(10px); /* Blur effect for frosted glass */
+        /* box-shadow: rgba(0, 0, 0, 0.15) 0px 4px 8px; */
+        padding: 20px;
+        border-radius: 15px;
+        margin-bottom: 30px;
+        transition: box-shadow 0.3s ease, transform 0.3s ease;
+        /* height: calc(100vh - 400px); */
+        height: 100%;
+    }
+
+    .vsection:hover {
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 8px 20px; /* Enhanced shadow effect */
+        transform: translateY(-5px); /* Slight lift on hover */
+        cursor: pointer ;
+    }
+
+    .vmago-section .container {
+        max-width: 1376px;
+    }
+
+    .vsection h2 {
+        font-size: 26px;
+    }
+
+    .vmago-section .container .row{
+        row-gap: 20px;
+    }
+
+    .vsection p{
+        font-size: 16px;
+    line-height: 26px;
+    min-height: 158px;    }
+
+    /* .row {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 30px;
+    }
+
+    .col-lg-3,
+    .col-md-6,
+    .col-sm-12 {
+        flex: 1 1 calc(25% - 30px);
+        /* Four items per row */
+        /* /min-width: 250px; */
+        /* display: flex; */
+</style>
+
+<section class="vmago-section">
+    <div class="container">
+        <h3>SERVICES</h3>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Our Authorship Services</h2>
+                    <p>Our experts fix all issues so that your submitted thesis is error-free, ensuring that your hard work is presented in the best possible light. We help you ensure that your thesis's content is grammatically accurate and has crisp punctuation, making it easy for the reader to understand and stay focused on the content. </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Research Consultancy</h2>
+                    <p>Emphasizing on exploring ideas to encourage the discipline in every possible way. On a global scale, we serves economically feasible and scalable research solutions to our clients. We pledge excellent and tailored support to meet the diverse needs as well as renders expert research and analysis to various institutions and organization at the worldwide level. </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Topics of Research</h2>
+                    <p>Research topics are forthcoming hubs, defines about the upcoming issues or research problems. Focus on organized and disseminated topics which maximizes the research impact by gaining attention into the community, and allow other people to read and discuss.
+                        
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Plagiarism Checker</h2>
+                    <p>To uphold the research integrity, identification of unauthorized work must be detected by  plagiarism checker online tools that deliver results within a seconds. By considering these tools, researchers can exclude complexity from their research.
+                       </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Research to Publication</h2>
+                    <p>To advance your knowledge and gaining attention from international and national recognition, manuscript or research writing and publications can strictly highlights institutions as well as your career. 
+                    Our research sheds light on a new perspective to assess the potential of upcoming
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Journal Subscription</h2>
+                    <p>Print and electronic journals have been the flagship service of V Mags Global since its inception. The company provides a comprehensive range of print journals in variety of subjects.We are providing print journals to leading academic and research institutions, with whom we enjoy a long standing relationship. 
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 offset-2">
+                <div class="vsection">
+                    <h2>Research Collaboration</h2>
+                    <p>Collaborative research act as conduit for groundbreaking discoveries and novel innovations, thus beneficial to researchers involved in the project. Interestingly, this approach combines several entities of different nature to focus the development, risk and ownership of results in executing the common project goals.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="vsection">
+                    <h2>Research Impact Services</h2>
+                    <p>Publishing your manuscript is an initial step. To get accessibility and visibility to your research, there is need to outreach your work to right audiences. VMAGO research impact services will support every researchers in upgrading their citations, attentions and connections.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
